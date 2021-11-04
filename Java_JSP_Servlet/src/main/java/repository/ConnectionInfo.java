d870eada1da2502fba354e58f29605419f4ca16e
SangSama <sang.sama.1999@gmail.com>
2021-11-04 07:29:43 +0700

    test errorr

---
+++ Java_JSP_Servlet/src/main/java/repository/ConnectionInfo.java
@@ -0,0 +1,16 @@
+package repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConnectionInfo {
+    private String url;
+    private String userName;
+    private String password;
+
+    public static void main(String[] args) {
+
+    }
+}
